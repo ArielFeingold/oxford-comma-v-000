1908e576ce98d51b6921c7d82b23b310b73f09bb
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 20:48:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
   new_array = []
-  long_array = []
-  last_one = array.last
+  last_one = " and #{array.last}"
   if array.count == 1
     array.join
   elsif array.count == 2
     array.join(" and ")
   else
     new_array == array.pop
-    long_array == new_array << " and #{last_one}"
-    long_array.join(", ")
+    array_string = new_array.join(", ")
+    array_string << last_one
+
   end
 end
