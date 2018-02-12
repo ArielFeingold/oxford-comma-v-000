df9c9a83a7e7822291f29c2307a67d3cc882ad5e
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 20:57:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   last_one = " and #{array.last}"
+      new_array = array.pop
   if array.count == 1
     array.join
   elsif array.count == 2
     array.join(" and ")
   else
-    new_array = array.pop
     string = new_array.join(", ")
     string << " and #{last_one}"
   end
